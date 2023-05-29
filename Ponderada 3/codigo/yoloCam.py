8397a816690ca4364c8a199bee36635b51caa818
ipatriciahonorato <patricia.moreira@sou.inteli.edu.br>
2023-05-29 03:15:11 -0300

    Update YoloCam.py

--- Ponderada 3/codigo/yoloCam.py
+++ Ponderada 3/codigo/yoloCam.py
@@ -2,5 +2,5 @@
 from ultralytics import YOLO #import the YOLO model
 import cv2 #import opencv
 
-model = YOLO("C:/Users/Inteli/OneDrive/Documents/GitHub/Modulo-6/ponderada 3/YoloModel/best.pt") #load the model
+model = YOLO("C:/Users/Inteli/OneDrive/Documents/GitHub/Modulo-6/ponderada 3/codigo/YoloModel/best.pt") #load the model
 model.predict(source="0", show = True, conf=0.5) #predict only images with a confidence of 50% and show the image
